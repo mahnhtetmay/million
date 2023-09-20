fc49f11338ab4c67a57b1f63996eec5da02b1e2e
mahnhtetmay <mahnhtetmay@gmail.com>
2023-09-20 23:47:48 +0630

    Edit Welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,63 +1,6 @@
 @extends('layouts.app')
 @section('content')
-{{-- <section class="about-section-four">
-    <div class="auto-container">
-        <div class="row">
-            <!-- Content Column -->
-            <div class="content-column col-xl-6 col-lg-6 wow fadeInRight" data-wow-delay="600ms">
-                <div class="inner-column">
-                    <div class="sec-title light">
-                        <span class="sub-title">About Company</span>
-                        <h2>Empowering your financial <br>freedom</h2>
-                        <div class="text">It is a long established fact that a reader will be distracted by the readablejk content of a page when looking at its layout. Lorem Ipsum simply dummyjl text</div>
-                    </div>
-                    <div class="row">
-                        <div class="info-block-four col-lg-6 col-md-6 col-sm-12">
-                            <div class="inner">
-                                <div class="icon-box"><i class="icon flaticon-puzzle"></i></div>
-                                <h4 class="title">Cyber Security</h4>
-                                <div class="text">It is a long established fact that a reader will be</div>
-                            </div>
-                        </div>
-                        <div class="info-block-four col-lg-6 col-md-6 col-sm-12">
-                            <div class="inner">
-                                <div class="icon-box"><i class="icon flaticon-production"></i></div>
-                                <h4 class="title">Our Mission</h4>
-                                <div class="text">It is a long established fact that a reader will be</div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="btn-box">
-                        <div class="auther-info">
-                            <figure class="thumb"><img src="{{asset('assets/images/resource/about-thumb4-1.jpg')}}" alt=""></figure>
-                            <h4 class="title">Eleanor Pena</h4>
-                            <span class="designation">Web Designer</span>
-                        </div>
-                        <a href="page-about.html" class="theme-btn btn-style-one"><span class="btn-title">Get Started <i class="fa fa-arrow-right"></i></span></a>
-                    </div>
-                </div>
-            </div>
 
-            <!-- Image Column -->
-            <div class="image-column col-xl-6 col-lg-6">
-                <div class="inner-column">
-                    <span class="icon-dots bounce-x"></span>
-                    <figure class="image-1 overlay-anim wow fadeInUp"><img src="{{asset('assets/images/resource/about4-1.jpg')}}" alt=""></figure>
-                    <figure class="image-2 overlay-anim wow fadeInLeft"><img src="{{asset('assets/images/resource/about4-2.jpg')}}" alt=""></figure>
-                    <div class="exp-box">
-                        <div class="inner" style="background-color: gold;">
-                            <div class="icon-box">
-                                <span class="icon fa fa-award"></span>
-                                <h2 class="title">230+</h2>
-                            </div>
-                            <p class="text">Project Complete</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section> --}}
 
 <section class="fun-fact-section-two pull-down">
     <div class="auto-container">
